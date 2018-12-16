4c05f444a72df8a16f8d6210bd05f09fb2da380c
troy.knapp <troy.knapp@gmail.com>
2018-12-15 22:16:04 -0600

    Adding a new js lib, and grouping libs into a single file. Adding event to watch for spell list to popup, and when it does it adds buttons for cantrips.

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,4 +1,13 @@
 $(window).ready(function(){
+    cantripEvent.addButton();
     var mf = ModalFactory;
     mf.initModal();
 });
+
+var cantripEvent = {
+    addButton : function() {
+        document.arrive(".ct-spells__content", function() {
+            $("[class$='spell__at-will']").parent().html('<button class="ct-theme-button ct-theme-button--filled ct-theme-button--interactive ct-button character-button character-button-block-small character-button--disabled"><span class="ct-spells-spell__at-will" style="color: #fefefe">At Will</span></button>');
+        });
+    }
+}
